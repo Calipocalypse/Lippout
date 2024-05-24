b7155c593cc23ef4a9ca9f8a556290b41b56ff7e
Calipocalypse <calipocalypse@gmail.com>
2024-05-24 17:04:50 +0200

    Word recognition + ipa conversion

--- src/LipFileManager/WavFile.cs
+++ src/LipFileManager/WavFile.cs
@@ -10,16 +10,26 @@ namespace LipFileManager
     {
         public double Length { get; set; } = 0;
         public string Base64 { get; set; } = String.Empty;
+        public byte[] Bytes { get; set; }
+        public float SampleRate { get; set; }
         public WavFile(byte[] bytes) {
             var dataLength = BitConverter.ToInt32(bytes, 40); 
             double audioLengthInSeconds = dataLength / GetBytesPerSecond(bytes);
             Length = audioLengthInSeconds;
             Base64 = Convert.ToBase64String(bytes);
+            Bytes = bytes;
+            SampleRate = GetSampleRate(bytes);
         }
         private double GetBytesPerSecond(byte[] bytes)
         {
             var bytesPerSecond = BitConverter.ToInt32(bytes, 28);
             return bytesPerSecond;
+        }
+
+        private float GetSampleRate(byte[] bytes)
+        {
+            var sampleRate = BitConverter.ToInt32(bytes, 24);
+            return sampleRate;
         }
     }
 }
---
+++ src/LipFileManager/WordToPhonem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.PhoneticMatching;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+
+namespace LipFileManager
+{
+    public static class WordToPhonem
+    {
+        public static string recoPhonemes;
+        public static string GetPronunciationFromText(string sentence)
+        {
+            var pronouncer = EnPronouncer.Instance;
+
+            var spllitedSentence = sentence.Split(' ');
+
+            var ipa = String.Empty;
+
+            foreach (var word in spllitedSentence)
+            {
+                var output = pronouncer.Pronounce(word);
+                ipa += output.Ipa + " ";
+            }
+
+            ipa = ipa.Trim();
+
+            return ipa;
+        }
+
+
+    }
+}
